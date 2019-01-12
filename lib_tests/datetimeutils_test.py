5a8a1ded7013b71596bf557b50a98d459e5b6ba7
Sielskyi Leonid <sielskyi.leonid@gmail.com>
2019-01-13 00:36:44 +0200

    Changed datetimeutils_test.py according to changes in datetimeext.py.

--- lib_tests/datetimeutils_test.py
+++ lib_tests/datetimeutils_test.py
@@ -1,15 +1,16 @@
-
-import datetimeutils as datetimeu
+import datetime
+from datetimeext import *
 
 def main():
 
-    datetimeu = DateTimeUtil()
-    datetimeu.set_date_time(2018, 9, 26, 23, 36)
-    print(datetimeu.get_date_time())
-    datetimeu.limit_datetime_to_max(2017, 10, 25, 12, 50)
-    print(datetimeu.get_date_time())
-    datetimeu.add_any(50, 1, 3, 10, 25)
-    print(datetimeu.get_date_time())
+    dte = DateTimeExt()
+    dte.set_date_time(2018, 9, 26, 23, 36)
+    assert (dte.get)
+    print(dte.get_date_time())
+    dte.limit_datetime_to_max(2017, 10, 25, 12, 50)
+    print(dte.get_date_time())
+    dte.add_any(50, 1, 3, 10, 25)
+    print(dte.get_date_time())
 
 if __name__ is '__main__':
     main()
